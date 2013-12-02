46ba8450f87b84a6b62d9a7886380c98494aa3a7
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-12-02 17:27:26 +0000

    Refactor code, delete unused functional.

--- qtsconverter.rb
+++ qtsconverter.rb
@@ -1,93 +1,58 @@
 #!/usr/bin/env ruby
 require 'csv'
 
-class MailChimpConvertor
+class MailChimpConverter
   attr_accessor :columns
 
   COLUMNS_HEADER = {
-    :industry => 'career interests',
-    :graduation_year => 'graduation_year',
-    :email => 'email',
-    :source => 'source',
-    :university => 'university',
+    :email => 'Email Address',
+    :industry => 'Career interests',
+    :graduation_year => 'Graduation year',
+    :source => 'Source',
   }
 
-  INDUSTRIES = [
-    'Accounting, Auditing, Tax',
-    'Advertising, Marketing & Public Relations',
-    'Banking & Financial Services, Trading',
-    'Education & Teaching',
-    'Engineering',
-    'Human Resources',
-    'Insurance & Actuarial',
-    'IT & Telecomunications',
-    'Law & Legal',
-    'Management Consulting',
-    'Public Sector & Civil Service',
-    # 'Government, Public Sector & Civil Service',
-    'Recruitment & Sales',
-    'Retail, Buying & FMCG',
-    'Science & Research',
-  ]
-  
-  GRADES = {
-    '1st'         => 'First',
-    '2:1'         => 'Upper Second',
-    '2:2'         => 'Lower Second',
-    '3rd'         => 'Third',
-    'Distinction' => 'Masters/PhD',
-    'Pass'        => 'Masters/PhD',
-    'Merit'       => 'Masters/PhD',
-    'Other'       => 'Other',
-  }
-  
-  def initialize(filename, source = '', university = '')
+  def initialize(options = {})
+    @files = options[:files]
     @columns = []
-    CSV.read(filename).each do |column|
-      email = column[5]
-      
-      @columns.push(
-        :industry => self.fix_industry(column[3]),
-        :graduation_year => column[4],
-        :email => email,
-        :source => source,
-        :university => university,
-      ) if email_is_valid?(email)
-    end
+    options[:files].each { |file| fill(file) }
   end
 
   def save
     CSV.open("output.csv", "wb") do |csv|
+      # Add header
       csv << COLUMNS_HEADER.values
-      
-      @columns.each do |column|
-        csv << column.values
-      end
+
+      @columns.each { |column| csv << column.values }
     end
   end
-  
+
+  private
+  def fill(file)
+    CSV.read(file).each do |column|
+      email = column[5]
+
+      @columns.push(
+        :email => email,
+        :industry => fix_industry(column[3]),
+        :graduation_year => column[4],
+        :source => File.basename(file, ".csv"),
+      ) if email_is_valid?(email)
+    end
+  end
+
   def email_is_valid?(email)
     !/\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/.match(email).nil?
   end
-    
+
   def fix_industry(value)
     # Escaping commas.
-    output = []
-    INDUSTRIES.each do |industry|
-      if value && value.index(industry)
-        output += [industry.gsub(/\,/, '\,')] # Replace ',' to '\,'
-      end
+    industries = []
+    CSV.parse_line(value).each do |industry|
+      industries += [industry.gsub(/\,/, '\,')] # Replace ',' to '\,'
     end
-    
-    return output.join(', ')
+
+    return industries.join(', ')
   end
-  
 end
 
-csv_file = ARGV[0]
-source = ARGV[1]
-university = ARGV[2]
-
-columns = MailChimpConvertor.new(csv_file, source, university)
-columns.save
-
+MailChimpConverter.new(files: ARGV).save
