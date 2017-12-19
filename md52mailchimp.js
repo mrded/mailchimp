1c608460975f8e5730e6c736a6fc4fc27d4d0c0f
Dmitry Demenchuk <dmitry@demenchuk.me>
2017-12-19 12:25:37 +0000

    Add script to match md5 hashes with mail list.

---
+++ md52mailchimp.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node 
+
+const fs = require('fs');
+
+const hashesPromise = new Promise(function(resolve, reject) {
+  const readline = require('readline');
+
+  let hashes = [];
+
+  readline.createInterface({
+    input: fs.createReadStream(process.argv[2]),
+  }).on('line', function(line) {
+    hashes.push(line);
+  }).on('close', function() {
+    resolve(hashes);
+  }).on('error', reject);
+}); 
+
+const emailsPromise = new Promise(function(resolve, reject) {
+  const csv = require('csv-parser')
+  const md5 = require('md5');
+
+  let emails = {};
+
+  fs.createReadStream(process.argv[3])
+    .pipe(csv())
+    .on('data', function(data) {
+      let email = data['Email Address'].toLowerCase().trim();
+      emails[md5(email)] = email;
+    })
+    .on('end', function() {
+      resolve(emails);
+    })
+    .on('error', reject);
+});
+
+emailsPromise.then(function(emails) {
+  return hashesPromise.then(function(hashes) {
+    for (hash of hashes) {
+      if (emails.hasOwnProperty(hash)) {
+        process.stdout.write(emails[hash] + "\n");
+      }
+    }
+  });
+});
